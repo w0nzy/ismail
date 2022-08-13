c1d2ba857604e8617730ff46ea954086145f34b7
Alperen Çavuş <105172848+w0nzy@users.noreply.github.com>
2022-08-14 01:22:54 +0300

    Add files via upload

---
+++ ismail.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def mile_to_km(mile: float) -> int:
+   bir_mile = 1.6
+   return int(mile*bir_mile)
+def km_to_mile(km: int) -> float:
+   bir_km = 0.62137
+   return km/bir_km
+
+print("\t\tSEÇENEKLER")
+print("\t\t KM TO MİL")
+print("\t\t MİL TO KM")
+print("\t\t yazabileceğin komutlar kmye çevirmek için km\nmile çevirmek için mil yazmanız gerekli")
+
+while True:
+    cvp = input("Neyi neyi çevirmek istiyorsunuz: ").lower()
+    cvp = cvp.split()
+    header = cvp[0]
+    if not header == '':
+       if header == "km":
+          km = int(input("Kilometre giriniz: "))
+          print("Girdiğiniz %s kilometre %s mil yapıyor" % (km,km_to_mile(km)))
+       elif header == "mil":
+          mil = float(input("Mil giriniz: "))
+          print("Girdiğiniz %s mil %s kilometre yapıyor" % (mil,mile_to_km(mil)))
+       else:
+          print("Geçersiz cevap: %s " % (header))
+    
